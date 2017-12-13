46d170cf19a5ea6fcfb156ebac7e2161435f7ad5
Eduardo <ecarrillosalvatierra@gmail.com>
2017-12-12 21:45:21 -0600

    Duplicated files data removed

--- 69-tbl-col-translation-drop-unnecessary-constraint.sql
+++
@@ -1,8 +0,0 @@
-declare
-    err exception;
-    pragma exception_init (err,-02443);
-begin
-    execute immediate 'alter table iff_tbl_col_translation drop constraint IFF_TBL_COL_TRANSLATION_UK1';
-    exception when err then null;
-end;
-/
